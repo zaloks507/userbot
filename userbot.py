95e0cfdbe64209ecaa365bafc2a39aeb6dcd479b
zaloks507 <bacokpripisnovogrehovskij@gmail.com>
2026-03-18 19:48:03 +0700

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -24,8 +24,9 @@ spam_time = {}
 
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
+STRING_SESSION = os.getenv("STRING_SESSION")
 
-app = Client("session", api_id=API_ID, api_hash=API_HASH)
+app = Client("session", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
 
 @app.on_message(filters.me & filters.command("help", prefixes='.'))
 def help(client, message):
